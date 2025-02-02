1c86355e183d67ac0ce01594b1e605f5f56fecfc
Kumar Satyam <ndakumarsatyam@gmail.com>
2025-02-02 20:18:19 +0530

    dfv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const mongoose = require('mongoose')
 const express = require('express')
 const workoutRoutes = require('./routes/workouts')
 
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
