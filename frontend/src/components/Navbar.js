ecd5c0dff8b3e4c29038c676992f83cf7cd4c009
Kumar Satyam <ndakumarsatyam@gmail.com>
2025-02-26 12:54:35 +0530

    someapi

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
 
+
 const Navbar = () => {
   const { logout } = useLogout()
   const { user } = useAuthContext()
@@ -13,18 +14,26 @@ const Navbar = () => {
   return (
     <header>
       <div className="container">
-        <Link to="/">
-          <h1>Workout Buddy</h1>
+        <Link to="/" className="logo">
+          <h1>Health Buddy</h1>
         </Link>
         <nav>
           {user && (
-            <div>
-              <span>{user.email}</span>
-              <button onClick={handleClick}>Log out</button>
+            <div className="nav-user-section">
+              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/exercises">Exercises</Link>
+              <Link to="/nutrition">Nutrition</Link>
+              <div className="user-controls">
+                <span className="user-email">{user.email}</span>
+                <button onClick={handleClick}>Log out</button>
+              </div>
             </div>
           )}
           {!user && (
-            <div>
+            <div className="nav-auth-section">
+              <Link to="/about">About</Link>
+              <Link to="/features">Features</Link>
               <Link to="/login">Login</Link>
               <Link to="/signup">Signup</Link>
             </div>
